fe5ec4e773038d4290d74963ab6c7f69c48ba71b
Jose <jvargas@basecampcodingacademy.org>
2018-03-30 11:17:01 -0500

    Finaly got connection to DB users and add new user

--- back_end/backend/src/main/java/com/example/backend/controllers/SessionsController.java
+++ back_end/backend/src/main/java/com/example/backend/controllers/SessionsController.java
@@ -1,16 +1,36 @@
 package com.example.backend.controllers;
 
 import com.example.backend.core.Credentials;
-import com.example.backend.core.User;
 import org.springframework.web.bind.annotation.*;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 //import sun.security.krb5.Credentials;
 
 @RestController
 public class SessionsController {
     @CrossOrigin()
     @PostMapping(path="/userSignup")
-    public User addNewUser (@RequestBody Credentials newUSer) {
-        return
+    public void addNewUser (@RequestBody Credentials newUSer) {
+
+        try (Connection conn = DriverManager.getConnection(
+                "jdbc:postgresql:iRide", "josevargas9817", "Everest1953");) {
+            PreparedStatement st = conn.prepareStatement("INSERT INTO users (f_name, l_name, usrnme, pw) VALUES (?, ?, ?, ?)");
+
+            st.setString(1, newUSer.f_name);
+            st.setString(2, newUSer.l_name);
+            st.setString(3, newUSer.usrnme);
+            st.setString(4, newUSer.pw);
+
+            int rowaadded = st.executeUpdate();
+            System.out.println("Row added: " + rowaadded);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
     }
 
 //    @GetMapping("/login")
