f021be3853b4bc7a7b4402595be97e72574cc15b
Jose <jvargas@basecampcodingacademy.org>
2018-04-12 10:57:04 -0500

    Log in, sign up, feed work fine. Cannot sell, cannot log out.

--- front_end/page/index.js
+++ front_end/page/index.js
@@ -4,12 +4,12 @@ function showSiteFront() {
   HTML +=
     '<img src="https://i.pinimg.com/originals/36/e1/f8/36e1f83d8d9d28866af60d584ea20925.jpg" alt="Fuel Hostage">';
   HTML +=
-    "<h4> Complete Wheel Set Ups Here </h4> <h4>Life Time Warrantee!</h4>";
+    "<h4> <a href='https://www.wheelsforless.com/offroad_wheels_and_tires.htm'><b>Complete Wheel Set Ups Here! </b></a></h4> <h4><b>Life Time Warrantee!</h4></b>";
   HTML += "</div><br>";
   var le_sde = "<h1>SPECS</h1>";
 
   le_sde += "<hr>";
-  le_sde += "<h2>Why you should do it!</h2>";
+  le_sde += "<h2><b>Why you should do it!</b></h2><hr>";
   le_sde += "<h3>* Upgrades your stance!</h3>";
   le_sde += "<h3>* Better clearance!</h3>";
   le_sde += "<h3>* Better pull!</h3>";
@@ -21,13 +21,17 @@ function showSiteFront() {
 function wheels(x) {
   var Wheels =
     '<p><a onclick="showWheel(' +
+    "'" +
     x.image +
+    "'" +
     ", " +
+    "'" +
     x.brand +
-    ", " +
-    x.dscrptn +
+    "'" +
     "," +
+    "'" +
     x.model +
+    "'" +
     ", " +
     x.price +
     ", " +
@@ -37,25 +41,60 @@ function wheels(x) {
     ' width="225" height="225"/></a></p>';
   return Wheels;
 }
-function showWheel(image, brand, dscrptn, model, price, stock) {
+
+function showWheel(image, brand, model, price, stock) {
+  console.log(image, brand, model, price, stock);
   var HTML =
     '<div id="js-data"><h1 id="branding">- Upgrade Your Stance Today! -</h1>';
   HTML += '<img id="wheelshwn" src=' + image + ' alt="Fuel Hostage">';
   HTML +=
-    "<h4> Complete Wheel Set Ups Here </h4> <h4>Life Time Warrantee!</h4>";
+    "<h4> <a href='https://www.wheelsforless.com/offroad_wheels_and_tires.htm'><b>Complete Wheel Set Ups Here! </b></a></h4> <h4><b>Life Time Warrantee!</h4></b>";
+  HTML +=
+    "<button id='ordbtn' onclick='buyWheel(" + stock + ")'> Order It!</button>";
   HTML += "</div><br>";
   var le_sde = "<h1>SPECS</h1>";
 
   le_sde += "<hr>";
-  le_sde += "<h2>" + dscrptn + "</h2>";
-  le_sde += "<h3>" + brand + "</h3>";
-  le_sde += "<h3>" + model + "</h3>";
-  le_sde += "<h3>" + price + "</h3>";
-  le_sde += "<h3>" + stock + "</h3>";
+  le_sde +=
+    "<img src='https://images.customwheeloffset.com/wheels/helo/he901/he901_black_white.jpg' width='40' height='40'><hr>";
+  le_sde += "<h3><b>Brand: </b>" + brand + "</h3><hr>";
+  le_sde += "<h3><b>Model: </b>" + model + "</h3><hr>";
+  le_sde += "<h3><b>Price(4): $</b>" + price + "</h3><hr>";
+  le_sde += "<h3 id='stck'><b>Stock: </b>" + stock + "</h3><hr>";
 
   $(".area-main").html(HTML);
   $(".de").html(le_sde);
 }
+
+function buyWheel(stock) {
+  $("#ordbtn").on("click", function() {
+    if (stock != 0) {
+      console.log(stock);
+      stock -= 1;
+    }
+  });
+}
+
+// function showWheel(image, brand, dscrptn, model, price, stock) {
+//   console.log(image, brand, dscrptn, model, price, stock);
+//   var HTML =
+//     '<div id="js-data"><h1 id="branding">- Upgrade Your Stance Today! -</h1>';
+//   HTML += '<img id="wheelshwn" src=' + image + ' alt="Fuel Hostage">';
+//   HTML +=
+//     "<h4> Complete Wheel Set Ups Here </h4> <h4>Life Time Warrantee!</h4>";
+//   HTML += "</div><br>";
+//   var le_sde = "<h1>SPECS</h1>";
+
+//   le_sde += "<hr>";
+//   le_sde += "<h2>" + dscrptn + "</h2>";
+//   le_sde += "<h3>" + brand + "</h3>";
+//   le_sde += "<h3>" + model + "</h3>";
+//   le_sde += "<h3>" + price + "</h3>";
+//   le_sde += "<h3>" + stock + "</h3>";
+
+//   $(".area-main").html(HTML);
+//   $(".de").html(le_sde);
+// }
 
 function showAllWheels() {
   $.ajax({
@@ -67,8 +106,13 @@ function showAllWheels() {
   })
     .then(function successfulShowWheels(response) {
       console.log(response);
+      var sell = response
+        .map(function(x) {
+          return buyWheel(x);
+        })
+        .join("");
       var wheel =
-        '<div id="wheel"><h1>Wheels</h1><hr></div>' +
+        '<div id="wheel"><h1><strong>WHEELS</strong></h1><hr></div>' +
         response
           .map(function(x) {
             return wheels(x);
